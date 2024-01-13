272d3201754b45df34dc95af669c37b365f1faa4
Justin <web@jstn.ly>
2024-01-14 00:07:37 +0700

    Update colors in motd.sh script

--- motd.sh
+++ motd.sh
@@ -2,7 +2,7 @@
 
 # Color codes
 red="\033[0;31m"
-yellow="\033[1;33m"
+yellow="\033[0;33m"
 green="\033[1;32m"
 blue="\033[1;34m"
 purple="\033[1;35m"
@@ -28,16 +28,17 @@ fi
 # Display the OS
 get_os() {
   os=$(cat /etc/os-release | grep PRETTY_NAME | cut -d "=" -f 2- | sed 's/"//g')
-  printf "${dark_grey}Welcome to $os\n"
+  printf "${dark_grey}Welcome to $os [0m\n"
 }
 
 # Display the ASCII header
 get_ascii_header() {
-  printf "${green} ________         __     _______                              \n"
-  printf "${green}|  |  |  |.-----.|  |--.|     __|.-----.----.--.--.-----.----.\n"
-  printf "${green}|  |  |  ||  -__||  _  ||__     ||  -__|   _|  |  |  -__|   _|\n"
-  printf "${green}|________||_____||_____||_______||_____|__|  \___/|_____|__|  \n\n"
-  printf "${reset_color}"
+  printf "%b\n" \
+  "[38;5;128m __[38;5;129m___[38;5;93m___[38;5;99m    [38;5;63m   [38;5;69m  _[38;5;33m_   [38;5;39m  _[38;5;38m___[38;5;44m___ [38;5;43m   [38;5;49m   [38;5;48m    [38;5;84m   [38;5;83m   [38;5;119m    [38;5;118m   [38;5;154m   [38;5;148m   " \
+  "[38;5;128m|  [38;5;129m|  [38;5;93m|  [38;5;99m|.-[38;5;63m----[38;5;69m.| [38;5;33m |--[38;5;39m.| [38;5;38m   [38;5;44m __[38;5;43m|.--[38;5;49m---[38;5;48m.--[38;5;84m--.-[38;5;83m-.-[38;5;119m-.--[38;5;118m---[38;5;154m.--[38;5;148m--." \
+  "[38;5;128m| [38;5;129m |  [38;5;93m|  [38;5;99m|| [38;5;63m -_[38;5;69m_|| [38;5;33m _ [38;5;39m ||[38;5;38m__  [38;5;44m   [38;5;43m||  [38;5;49m-__[38;5;48m|  [38;5;84m _| [38;5;83m | [38;5;119m | [38;5;118m -_[38;5;154m_|  [38;5;148m _|" \
+  "[38;5;128m|_[38;5;129m___[38;5;93m___[38;5;99m_||_[38;5;63m___[38;5;69m_||[38;5;33m____[38;5;39m_||[38;5;38m___[38;5;44m____[38;5;43m||_[38;5;49m___[38;5;48m_|__[38;5;84m|  [38;5;83m\__[38;5;119m_/|_[38;5;118m___[38;5;154m_|_[38;5;148m_|  " \
+  "[0m"
 }
 
 # Display the uptime
@@ -87,7 +88,7 @@ get_ram_usage() {
 # Display a text-based progress bar for disk space
 get_disk_space() {
   printf "\n%-20s %-10s %-10s %-10s %-10s %-10s\n" "Filesystems" "Size" "Used" "Avail" "Use%" "Mounted"
-  df -BG | awk 'NR>1 {print $1, $2, $3, $4, $5, $6}' | grep -vE 'tmpfs|devtmpfs|boot' | while read -r filesystem size used avail use mounted; do
+  df -BG | awk 'NR>1 {print $1, $2, $3, $4, $5, $6}' | grep -vE 'tmpfs|devtmpfs|boot|mnt|run|init' | while read -r filesystem size used avail use mounted; do
     printf "%-20s %-10s %-10s %-10s %-10s %-10s\n" "$filesystem" "$size" "$used" "$avail" "$use" "$mounted"
     used_space=${used%G}
     total_space=${size%G}
@@ -108,12 +109,12 @@ generate_progress_bar() {
   # Check percentage, if under 50% set color to green, 
   # if under 75% set color to yellow, 
   # else set color to red.
-  if [ "$percentage" -lt 50 ]; then
-    color_code="\033[0;32m"
-  elif [ "$percentage" -lt 75 ]; then
-    color_code="\033[1;33m"
+  if [ "$percentage" -lt 75 ]; then
+    color_code=$green
+  elif [ "$percentage" -lt 90 ]; then
+    color_code=$yellow
   else
-    color_code="\033[0;31m"
+    color_code=$red
   fi
 
   # Create the progress bar string with color codes
@@ -132,16 +133,15 @@ generate_progress_bar() {
 get_fail2ban_status() {
   fail2ban_status=$(systemctl status fail2ban | grep Active | awk '{print $2}')
   if [ "$fail2ban_status" == "active" ]; then
-    color=${green}
+    printf "Fail2ban: ${color}Online${reset_color}\n"
   else
-    color=${red}
+    printf "Fail2ban: ${color}Offline${reset_color}\n"
   fi
-  printf "Fail2ban: ${color}Online${reset_color}\n"
 }
 
 get_fail2ban_count() {
   fail2ban_count=$(fail2ban-client status | grep "Number of jail:" | awk '{print $5}')
-  printf "Fail2ban Count: ${green}$fail2ban_count${reset_color}\n"
+  printf "IP's jailed: ${green}$fail2ban_count${reset_color}\n"
 }
 
 # Display the MOTD
