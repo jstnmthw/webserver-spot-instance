4583682431f0a28e92d443f7f406673d1aad091a
Justin <web@jstn.ly>
2024-05-13 22:48:36 +0700

    refactor: Update gameserver installation script to include server-example.sh with tfc classic configuration

--- gameserver.sh
+++ gameserver.sh
@@ -5,4 +5,4 @@ git clone https://github.com/jstnmthw/srcds-autoinstall.git ~/srcds-autoinstall
 cd ~/srcds-autoinstall
 touch .env
 
-make install SCRIPT=server-example.sh GAME=tfc CONFIG=classic CONTAINER=tfc
+make install SCRIPT=server-example.sh GAME=tfc CONFIG=classic CONTAINER=tfc1
