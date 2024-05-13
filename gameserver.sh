42be06a41e91c06f12dd6e0f6e39a40bd4dc0695
Justin <web@jstn.ly>
2024-05-13 22:13:59 +0700

    refactor: Add .env file to gameserver installation script

--- gameserver.sh
+++ gameserver.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/jstnmthw/srcds-autoinstall.git ~/srcds-autoinstall
 
 cd ~/srcds-autoinstall
+touch .env
 
 make install SCRIPT=server-example.sh GAME=tfc CONFIG=classic CONTAINER=tfc
