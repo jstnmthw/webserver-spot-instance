bdcf1d38f47c0620f6cb9642bdbf91e88b1f8545
Justin <web@jstn.ly>
2024-10-02 01:14:59 +0700

    Refactor shell prompt customization in installation script

--- gameserver.sh
+++ gameserver.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-git clone https://github.com/jstnmthw/srcds-autoinstall.git ~/srcds-autoinstall
+# If arguments 1 exit with a message
+if [ -z "$1" ]; then
+  echo "Usage: $0 <username>"
+  exit 1;
+fi
 
-cd ~/srcds-autoinstall
+# Username
+username=$1
+
+su - $username -c "git clone https://github.com/jstnmthw/srcds-autoinstall.git /home/$username/srcds-autoinstall"
+
+cd /home/$username/srcds-autoinstall
 touch .env
 
 make install SCRIPT=cs2-example
--- install.sh
+++ install.sh
@@ -67,7 +67,7 @@ fi
 sudo usermod -aG docker $default_user
 
 # Customize the shell prompt to display the username and hostname
-echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;48m\]:\[\033[\033[38;5;30m\]\w \033[38;5;48m\]\$ \033[0m\]\"" >> /home/$default_user/.bashrc
+echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;48m\]:\[\033[\033[38;5;30m\]\w \033[38;5;48m\]\$ \033[0m\] \"" >> /home/$default_user/.bashrc
 
 # Check if user exists, if not create it, copy SSH key and add to docker group
 if id "$username" >/dev/null 2>&1; then
@@ -88,7 +88,7 @@ else
   echo "${username} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
 
   # Customize the shell prompt to display the username and hostname
-  echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;48m\]:\[\033[\033[38;5;30m\]\w \033[38;5;48m\]\$ \033[0m\]\"" >> /home/$username/.bashrc
+  echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;48m\]:\[\033[\033[38;5;30m\]\w \033[38;5;48m\]\$ \033[0m\] \"" >> /home/$username/.bashrc
 fi
 
 # Install docker-compose
@@ -159,7 +159,7 @@ if [ "$sever_type" == 2 ] || [ "$server_type" == 3 ]; then
   echo "Done."
   chmod +x /tmp/gameserver.sh
   cd /tmp
-  sudo ./gameserver.sh
+  sudo ./gameserver.sh $username
 fi
 
 # Set sshd to listen on port 666
