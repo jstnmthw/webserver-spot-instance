16fb665a693bf103f4e27eecf7342fe64188117d
Justin <web@jstn.ly>
2024-10-02 21:59:33 +0700

    Refactor shell prompt customization in installation script: Update shell prompt color in PS1

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ else
   echo "${username} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
 
   # Customize the shell prompt to display the username and hostname
-  echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;48m\]:\[\033[\033[38;5;30m\]\w \033[38;5;48m\]\$ \033[0m\] \"" >> /home/$username/.bashrc
+  echo "PS1=\"\033[38;5;48m\]\u\033[38;5;42m\]@\033[38;5;36m\]\h\033[38;5;29m\]:\[\033[\033[38;5;30m\]\w \033[0m\]\$ \"" >> /home/$username/.bashrc
 fi
 
 # Install docker-compose
