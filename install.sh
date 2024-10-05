06217c9f2065e283fdd163ecf271c923ce04daae
Justin <web@jstn.ly>
2024-10-05 14:50:01 +0700

    Refactor installation script: Update firewall rules, enable ufw, and set sshd to listen on port 666

--- install.sh
+++ install.sh
@@ -1,176 +1,211 @@
 #!/bin/bash
 
-# If arguments 1, 2, 3 & 4 are not provided, exit with a message
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-  echo "Usage: $0 <username> <aws_bucket_name> <aws_region> <server_type>"
-  exit 1;
-fi
+# Function to check if the script is run as root
+check_root() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Please run as root or use sudo."
+    exit 1
+  fi
+}
 
-# Username
-username=$1
+# Function to check if the required arguments are provided
+check_arguments() {
+  if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+    echo "Usage: $0 <username> <aws_bucket_name> <aws_region> <server_type>"
+    exit 1
+  fi
+}
 
-# AWS bucket name
-aws_bucket_name=$2
+# Function to set the package manager and default user
+set_package_manager() {
+  if command -v yum >/dev/null 2>&1; then
+    package_manager="yum"
+    default_user="ec2-user"
+  elif command -v apt-get >/dev/null 2>&1; then
+    package_manager="apt-get"
+    default_user="ubuntu"
+  else
+    echo "No package manager found. Exiting..."
+    exit 1
+  fi
+}
 
-# AWS region
-aws_region=$3
+# Function to set the hostname based on server type
+set_hostname() {
+  local server_type="$1"
+  if command -v hostnamectl >/dev/null 2>&1; then
+    case "$server_type" in
+      1) hostnamectl set-hostname webserver ;;
+      2|3) hostnamectl set-hostname gameserver ;;
+      *) hostnamectl set-hostname server ;;
+    esac
+  else
+    echo "No hostnamectl command found. Skipping..."
+  fi
+}
 
-# Type - webserver, gameserver or both
-server_type=$4
+# Function to update and install necessary packages
+update_and_install_packages() {
+  $package_manager update -y
+  $package_manager install docker.io git iftop fail2ban -y
+  service docker start
+  snap install aws-cli --classic
+  if [ "$server_type" == "2" ] || [ "$server_type" == "3" ]; then
+    $package_manager install make -y
+  fi
+}
 
-# Default user
+# Function to add a user to the docker group
+add_user_to_docker_group() {
+  local user="$1"
+  usermod -aG docker "$user"
+}
+
+# Function to customize the shell prompt for a user
+customize_shell_prompt() {
+  local user_home="$1"
+  echo 'PS1="\[\033[38;5;48m\]\u\[\033[38;5;42m\]@\[\033[38;5;36m\]\h\[\033[38;5;29m\]:\[\033[38;5;30m\]\w \[\033[0m\]\$ "' >> "$user_home/.bashrc"
+}
+
+# Function to create a new user if it doesn't exist
+create_user() {
+  local username="$1"
+  if id "$username" >/dev/null 2>&1; then
+    echo "User '$username' already exists. Skipping..."
+  else
+    useradd -m -s /bin/bash "$username"
+    add_user_to_docker_group "$username"
+    mkdir -p /home/"$username"/.ssh
+    cp /home/"$default_user"/.ssh/authorized_keys /home/"$username"/.ssh/
+    chown -R "$username":"$username" /home/"$username"/.ssh
+    usermod -aG sudo "$username"
+    cp /etc/skel/.bashrc /home/"$username"/.bashrc
+    echo "${username} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/"$username"
+    customize_shell_prompt "/home/$username"
+  fi
+}
+
+# Function to install Docker Compose
+install_docker_compose() {
+  local docker_compose_version
+  docker_compose_version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+  curl --retry 3 --retry-delay 5 -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+}
+
+# Function to associate an Elastic IP with the instance
+associate_elastic_ip() {
+  aws configure set default.region "$aws_region"
+  aws_elastic_ip=$(aws s3 cp s3://${aws_bucket_name}/elastic-ip.txt - | tr -d '\r')
+  aws_token=$(curl --silent --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
+  aws_instance_id=$(curl --silent "http://169.254.169.254/latest/meta-data/instance-id" --header "X-aws-ec2-metadata-token: $aws_token")
+  aws ec2 associate-address --instance-id "$aws_instance_id" --public-ip "$aws_elastic_ip"
+}
+
+# Function to set up UFW firewall rules
+setup_firewall() {
+  ufw default allow outgoing
+  ufw default deny incoming
+  ufw allow 666/tcp
+  ufw allow 80/tcp
+  ufw allow 443/tcp
+  if [ "$server_type" == "2" ] || [ "$server_type" == "3" ]; then
+    ufw allow 27015:27030/udp
+    ufw allow 27015:27030/tcp
+  fi
+  ufw --force enable
+}
+
+# Function to set up Fail2Ban
+setup_fail2ban() {
+  cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+  sed -i 's/bantime  = 10m/bantime = 1h/g' /etc/fail2ban/jail.local
+  sed -i 's/findtime  = 10m/findtime = 1h/g' /etc/fail2ban/jail.local
+  sed -i 's/maxretry = 5/maxretry = 3/g' /etc/fail2ban/jail.local
+  systemctl enable fail2ban
+  systemctl start fail2ban
+}
+
+# Function to install a custom MOTD
+install_custom_motd() {
+  echo "Setting up the MOTD..."
+  chmod -x /etc/update-motd.d/*
+  rm /etc/update-motd.d/*landscape*
+  chmod +x /etc/update-motd.d/*updates-available
+  chmod +x /etc/update-motd.d/*reboot-required
+  curl -s https://raw.githubusercontent.com/jstnmthw/webserver-spot-instance/master/motd.sh > /tmp/motd.sh
+  chmod +x /tmp/motd.sh
+  mv /tmp/motd.sh /etc/update-motd.d/00-motd
+  echo "Done."
+}
+
+# Function to disable ESM update messages
+disable_esm_update_motd() {
+  echo "Disabling ESM update motd..."
+  sed -Ezi.orig \
+    -e 's/(def _output_esm_service_status.*?:\n)/\1    return\n/' \
+    -e 's/(def _output_esm_package_alert.*?:\n)/\1    return\n/' \
+    /usr/lib/update-notifier/apt_check.py
+  /usr/lib/update-notifier/update-motd-updates-available --force
+  echo "Done."
+}
+
+# Function to update MOTD if using yum
+update_motd_if_yum() {
+  if command -v yum >/dev/null 2>&1; then
+    update-motd
+  fi
+}
+
+# Function to set up the gameserver
+setup_gameserver() {
+  if [ "$server_type" == "2" ] || [ "$server_type" == "3" ]; then
+    echo "Downloading gameserver setup script..."
+    curl -s https://raw.githubusercontent.com/jstnmthw/webserver-spot-instance/master/gameserver.sh > /tmp/gameserver.sh
+    chmod +x /tmp/gameserver.sh
+    cd /tmp || exit
+    ./gameserver.sh "$username"
+    echo "Done."
+  fi
+}
+
+# Function to configure SSHD
+configure_and_restart_sshd() {
+  sed -i 's/#Port 22/Port 666/g' /etc/ssh/sshd_config
+  systemctl enable ssh
+  systemctl restart ssh
+}
+
+# Function to upgrade packages and reboot
+upgrade_and_reboot() {
+  $package_manager upgrade -y && reboot
+}
+
+# Checks
+check_root
+check_arguments "$@"
+
+# Assign variables
+username="$1"
+aws_bucket_name="$2"
+aws_region="$3"
+server_type="$4"
 default_user="root"
 
-# Detect apt or yum
-if [ -n "$(command -v yum)" ]; then
-  package_manager="yum"
-  default_user="ec2-user"
-elif [ -n "$(command -v apt-get)" ]; then
-  package_manager="apt-get"
-  default_user="ubuntu"
-else
-  echo "No package manager found. Exiting..."
-  exit 1;
-fi
-
-# Set the hostname
-if [ -n "$(command -v hostnamectl)" ]; then
-  if [ "$server_type" == 1 ]; then
-    sudo hostnamectl set-hostname webserver
-  elif [ "$server_type" == 2 ] || [ "$server_type" == 3 ]; then
-    sudo hostnamectl set-hostname gameserver
-  else
-    sudo hostnamectl set-hostname server
-  fi
-else
-  echo "No hostnamectl command found. Skipping..."
-fi
-
-# Update the instance
-sudo $package_manager update -y
-
-# Install Docker
-sudo $package_manager install docker.io -y
-sudo service docker start
-
-# Install services
-sudo $package_manager install git iftop fail2ban -y
-
-# Install AWS CLI
-sudo snap install aws-cli --classic
-
-if [ "$sever_type" == 2 ] || [ "$server_type" == 3 ]; then
-  sudo $package_manager install make -y
-fi
-
-# Add root user to the docker group
-sudo usermod -aG docker $default_user
-
-# Customize the shell prompt to display the username and hostname
-echo 'PS1="\[\033[38;5;48m\]\u\[\033[38;5;42m\]@\[\033[38;5;36m\]\h\[\033[38;5;29m\]:\[\033[38;5;30m\]\w \[\033[0m\]\$ "' >> /home/$default_user/.bashrc
-
-# Check if user exists, if not create it, copy SSH key and add to docker group
-if id "$username" >/dev/null 2>&1; then
-  echo "User already exists. Skipping..."
-else
-  # Install user and add to docker group
-  useradd -m -s /bin/bash $username
-  sudo usermod -a -G docker $username
-
-  # Copy the SSH authorized key from the default user to the newly created user
-  mkdir -p /home/$username/.ssh
-  cp /home/$default_user/.ssh/authorized_keys /home/$username/.ssh/
-  chown -R $username:$username /home/$username/.ssh
-
-  # Add the new user to the sudoers group (optional, for administrative privileges)
-  usermod -aG sudo $username
-  cp /etc/skel/.bashrc /home/$username/.bashrc
-  echo "${username} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
-
-  # Customize the shell prompt to display the username and hostname
-  echo 'PS1="\[\033[38;5;48m\]\u\[\033[38;5;42m\]@\[\033[38;5;36m\]\h\[\033[38;5;29m\]:\[\033[38;5;30m\]\w \[\033[0m\]\$ "' >> /home/$username/.bashrc
-fi
-
-# Install docker-compose
-docker_compose_version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-sudo curl --retry 3 --retry-delay 5 -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Associate Elastic IP
-aws configure set default.region $aws_region
-aws_elastic_ip=$(aws s3 cp s3://${aws_bucket_name}/elastic-ip.txt - | tr -d '\r')
-aws_token=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
-aws_instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id --header "X-aws-ec2-metadata-token: $aws_token")
-aws ec2 associate-address --instance-id $aws_instance_id --public-ip $aws_elastic_ip
-
-# Setup ufw firewall with deny rules and then allow rules
-sudo ufw default allow outgoing
-sudo ufw default deny incoming
-sudo ufw allow 666/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-
-# If gameserver open srcds ports 27015-27030
-if [ "$sever_type" == 2 ] || [ "$server_type" == 3 ]; then
-  sudo ufw allow 27015:27030/udp
-  sudo ufw allow 27015:27030/tcp
-fi
-
-# Enable firewall
-sudo ufw --force enable
-
-# Set sshd to listen on port 666
-sed -i 's/#Port 22/Port 666/g' /etc/ssh/sshd_config
-sudo systemctl enable ssh
-sudo systemctl restart ssh
-
-# Setup fail2ban
-sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-sudo sed -i 's/bantime = 12h/bantime = 1h/g' /etc/fail2ban/jail.local
-sudo sed -i 's/findtime = 1m/findtime = 1h/g' /etc/fail2ban/jail.local
-sudo sed -i 's/maxretry = 3/maxretry = 3/g' /etc/fail2ban/jail.local
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
-
-# Download and install custom motd from the repo and copy it to /etc/update-motd.d/00-motd
-sudo chmod -x /etc/update-motd.d/*
-sudo rm /etc/update-motd.d/*landscape*
-sudo chmod +x /etc/update-motd.d/*updates-available
-sudo chmod +x /etc/update-motd.d/*reboot-required
-
-echo "Setting up the MOTD..."
-curl -s https://raw.githubusercontent.com/jstnmthw/webserver-spot-instance/master/motd.sh > /tmp/motd.sh
-chmod +x /tmp/motd.sh
-sudo mv /tmp/motd.sh /etc/update-motd.d/00-motd
-echo "Done."
-
-echo "Disabling ESM update motd..."
-sudo sed -Ezi.orig \
-  -e 's/(def _output_esm_service_status.outstream, have_esm_service, service_type.:\n)/\1    return\n/' \
-  -e 's/(def _output_esm_package_alert.*?\n.*?\n.:\n)/\1    return\n/' \
-  /usr/lib/update-notifier/apt_check.py
-sudo /usr/lib/update-notifier/update-motd-updates-available --force
-echo "Done."
-
-if [ -n "$(command -v yum)" ]; then  
-  sudo update-motd
-fi
-
-# Execute webserver setup script
-# if [ "$sever_type" == 1 ]; then
-  # TODO: Implement webserver setup script
-  # sudo ./site.sh $username
-# fi
-
-# Execute gameserver setup script
-if [ "$sever_type" == 2 ] || [ "$server_type" == 3 ]; then
-  echo "Downloading gameserver setup script..."
-  curl -s https://raw.githubusercontent.com/jstnmthw/webserver-spot-instance/master/gameserver.sh > /tmp/gameserver.sh
-  echo "Done."
-  chmod +x /tmp/gameserver.sh
-  cd /tmp
-  sudo ./gameserver.sh $username
-fi
-
-# Upgrade and reboot
-sudo $package_manager upgrade -y && sudo reboot
+# Main script executions
+set_package_manager
+set_hostname "$server_type"
+update_and_install_packages
+add_user_to_docker_group "$default_user"
+customize_shell_prompt "/home/$default_user"
+create_user "$username"
+install_docker_compose
+install_custom_motd
+disable_esm_update_motd
+update_motd_if_yum
+setup_firewall
+setup_fail2ban
+configure_and_restart_sshd
+associate_elastic_ip
+setup_gameserver
+upgrade_and_reboot
